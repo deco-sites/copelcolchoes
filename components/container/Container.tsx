8e90fbfde2e31f8e30fb09680fe1a49e1164972e
Mateus Martins Pinheiro <mateusmartinsmck@gmail.com>
2023-10-28 16:37:54 -0300

    fix: container width on mobile

--- components/container/Container.tsx
+++ components/container/Container.tsx
@@ -53,7 +53,7 @@ function Container({ sections }: Props) {
         >
           {withContainer
             ? (
-              <div class="container max-w-[80rem] w-full m-auto px-[4rem]">
+              <div class="container lg:max-w-[80rem] w-full m-auto px-[4rem]">
                 <Component {...props} />
               </div>
             )
