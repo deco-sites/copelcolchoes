078f5d7d83b648c65ea557a6b8e7ec6633fc03d5
Murilo Gabriel <murilofer@yahoo.com.br>
2025-03-11 13:33:02 -0300

    11758 - fix: atualizando selos da pdp

--- components/product/ProductDetails.tsx
+++ components/product/ProductDetails.tsx
@@ -362,51 +362,44 @@ function ImageComponent(
             alt={value}
           />
         </div>
-        <div class="text-primary text-sm text-center max-w-[75px]">{value}</div>
+        <div class="text-primary text-sm text-center">{value}</div>
       </div>
     )
     : <></>;
 }
 
 function Selos({ product }: { product: Product }) {
-  const { isVariantOf, category: fullCategory } = product;
+  const { isVariantOf } = product;
   const { additionalProperty } = isVariantOf as unknown as Product;
-  const category = fullCategory ? fullCategory.split(">")[0] : "";
-  const activeSelos = {
-    "Colchões": 7,
-    "Cama Box": 7,
-    "Cama Box mais Colchão": 7,
-    "Travesseiros": 7,
-    "Acessórios": 7,
-    "Móveis": 7,
-  };
+
+  const filteredProperties = additionalProperty?.filter(
+    (property) => property.propertyID === "Modelo",
+  );
 
   return (
     <div class="w-fit bg-transparent">
-      
-      {activeSelos[category as keyof typeof activeSelos] > 0 && (
-        <>
-          <div class="text-primary text-lg leading-8 font-semibold my-6">
-            <p>Informações do seu produto:</p>
-          </div>
-          <div class="flex items-start flex-row gap-4 justify-between">
-            {additionalProperty && additionalProperty.map((prop, index) => {
+      <div class="text-primary text-lg leading-8 font-semibold my-6">
+        <p>Informações do seu produto:</p>
+      </div>
+      <div class="flex items-start flex-wrap flex-row gap-4 sm:gap-8 justify-center">
+        {filteredProperties && filteredProperties.map((prop) => {
+          const { value } = prop;
 
-              const { value } = prop;
-              const url = value ? value.normalize("NFD").replaceAll("/ ", "").replace( /[\u0300-\u036f]/g,"",).replaceAll(" ", "_").toLowerCase(): "";
+          const url = value
+            ? value.normalize("NFD").replaceAll("/ ", "").replace(
+              /[\u0300-\u036f]/g,
+              "",
+            ).replaceAll(" ", "_").toLowerCase()
+            : "";
 
-              if (index >= activeSelos[category as keyof typeof activeSelos]) return;
-
-              return (
-                <ImageComponent
-                  imageUrl={`/arquivos/icone_${url}.svg`}
-                  value={value}
-                />
-              );
-            })}
-          </div>
-        </>
-      )}
+          return (
+            <ImageComponent
+              imageUrl={`/arquivos/icone_${url}.svg`}
+              value={value}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
