09d18ac8080e90a672910e5410a4f6fa36c3b7b4
Murilo Gabriel <murilofer@yahoo.com.br>
2025-06-05 11:14:01 -0300

    12079 - feat: adicionar novos alertas ao TopNavBar e ajustar a estrutura do componente

--- components/header/TopNavBar.tsx
+++ components/header/TopNavBar.tsx
@@ -1,8 +1,10 @@
+import Slider from "$store/components/ui/Slider.tsx";
+import SliderJS from "$store/islands/SliderJS.tsx";
+import { useId } from "preact/hooks";
+
 export interface Props {
-  /**
-   * @description Alerts available in the top nav bar
-   */
-  alert: Alert;
+  alerts: Alert[];
+  interval?: number;
 }
 
 export interface Alert {
@@ -11,16 +13,60 @@ export interface Alert {
   textColor: string;
 }
 
-function TopNavBar({ alert }: Props) {
+function TopNavBar({ alerts, interval =  2000 }: Props) {
+  const id = useId();
+
   return (
-    <div class="py-4 px-[1.875rem] lg:py-2 topnavbar bg-primary">
-      <div class="flex items-center justify-center">
-        <span
-          class="text-sm ml-3 text-center leading-[1.125rem]"
-          style={{ color: alert.textColor }}
+    <div class="py-4 px-[1.875rem] lg:py-2 topnavbar bg-primary relative group">
+      <div id={id} class="relative">
+        <Slider
+          class="carousel carousel-center w-full"
+          data-slider
         >
-          {alert.textAlert}
-        </span>
+          {alerts.map((alert, index) => (
+            <Slider.Item
+              index={index}
+              class="carousel-item w-full"
+              data-slider-item={index}
+            >
+              <div class="flex items-center justify-center w-full">
+                <span
+                  class="text-sm ml-3 text-center leading-[1.125rem]"
+                  style={{ color: alert.textColor }}
+                >
+                  {alert.textAlert}
+                </span>
+              </div>
+            </Slider.Item>
+          ))}
+        </Slider>
+
+        
+        <div class="absolute top-1/2 left-0 right-0 flex justify-between px-2 transform -translate-y-1/2">
+          <button
+            class="btn btn-circle btn-sm"
+            data-slide="prev"
+            aria-label="Previous"
+          >
+            ❮
+          </button>
+          <button
+            class="btn btn-circle btn-sm"
+            data-slide="next"
+            aria-label="Next"
+          >
+            ❯
+          </button>
+        </div>
+
+        <SliderJS
+          rootId={id}
+          itemsPerPage={{ 0: 1 }}
+          interval={interval}
+          infinite
+          scroll="auto"
+          data-slider-controller-js
+        />
       </div>
     </div>
   );
